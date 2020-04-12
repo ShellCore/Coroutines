6bb7354da02fd66fccb15c7d2cf1ee405675408d
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-04-11 21:43:14 -0500

    Creación de ejemplo con contexto de la corutina

---
+++ 1_Coroutines_Basics/src/main/kotlin/Context.kt
@@ -0,0 +1,13 @@
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+
+    runBlocking {
+        launch(CoroutineName("myCoroutine")) {
+            println("This is run from ${coroutineContext[CoroutineName.Key]}")
+        }
+    }
+}
