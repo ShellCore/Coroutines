df804ef06b4540f56b45fab99d3da9ac250cf778
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-04-19 11:04:59 -0500

    Se agregó configuración para consulta de servicio.

--- 6_Android_Flow_Retrofit/app/src/main/java/page/shellcore/tech/android/a6androidflowretrofit/model/NewsArticle.kt
+++ 6_Android_Flow_Retrofit/app/src/main/java/page/shellcore/tech/android/a6androidflowretrofit/model/NewsArticle.kt
@@ -1,10 +1,13 @@
 package page.shellcore.tech.android.a6androidflowretrofit.model
 
+import com.google.gson.annotations.SerializedName
+
 data class NewsArticle(
     val author: String? = null,
     val title: String? = null,
     val description: String? = null,
     val url: String? = null,
+    @SerializedName("imageUrl")
     val urlToImage: String? = null,
     val publishedAt: String? = null
 )
