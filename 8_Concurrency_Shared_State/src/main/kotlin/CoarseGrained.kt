859089b766872188791cc59371837cb095c2df4c
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-04-19 16:01:34 -0500

    Solución con confinamiento de hilos.

---
+++ 8_Concurrency_Shared_State/src/main/kotlin/CoarseGrained.kt
@@ -0,0 +1,30 @@
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    runBlocking {
+        val counterContext = newSingleThreadContext("CounterContext")
+        var counter = 0
+        withContext(counterContext) {
+            massiveRun { counter++ }
+        }
+        println("Counter = $counter")
+    }
+}
+
+private suspend fun massiveRun(action: suspend () -> Unit) {
+    val n = 100
+    val k = 1000
+    val time = measureTimeMillis {
+        coroutineScope {
+            repeat(n) {
+                launch {
+                    repeat(k) {
+                        action()
+                    }
+                }
+            }
+        }
+    }
+    println("Completed ${n * k} actions in $time ms")
+}
